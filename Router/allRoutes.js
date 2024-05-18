29d7f6018fd6ba685bb2c932a2be10b1b1d56b49
Ashura1234 <156065272+Ashura1234@users.noreply.github.com>
2024-05-18 16:04:41 +0200

    ajout page restauration

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,6 +5,11 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/habitat", "Habitat", "/pages/habitat.html"),
     new Route("/services", "Services", "/pages/services.html"),
+    new Route("/detailTigre", "Habitat du tigre", "/pages/animaux/detailTigre.html"),
+    new Route("/detailZebre", "Habitat du Zebre", "/pages/animaux/detailZebre.html"),
+    new Route("/detailGirafe", "Habitat de la Girafe", "/pages/animaux/detailGirafe.html"),
+    new Route("/detailKoala", "Habitat du Koala ", "/pages/animaux/detailKoala.html"),
+    new Route("/reservation", "reserver", "/pages/reservation.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
