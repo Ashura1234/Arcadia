d3394b1ea5b5234cc6b34418b052005a3b4f22d7
Ashura1234 <156065272+Ashura1234@users.noreply.github.com>
2024-07-12 11:21:29 +0200

    ajout du token fictif

--- js/auth/signin.js
+++ js/auth/signin.js
@@ -7,8 +7,9 @@ btnConnexion.addEventListener("click", checkAuth);
 function checkAuth(){
     //appel de l'api pour vérifier les informations utilisateur en BDD
 
-    if (mailInput.value == "test@mail.com" && passwordInput.value =="123"){
-        alert("vous êtes connecté")
+    if (mailInput.value == "test@mail.com" && passwordInput.value =="123"){ 
+        const token = "eijiojoijrijihjroijhirjhoirjhoirjhiorjhoirjhtijrtoiorijtei";
+        setToken(token);
 
         window.location.replace("/")
     }else{
