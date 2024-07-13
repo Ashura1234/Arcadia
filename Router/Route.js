a22e993d3f3a404ad4c6cb25f5ab8e64c2bd2371
Ashura1234 <156065272+Ashura1234@users.noreply.github.com>
2024-07-13 12:07:30 +0200

    [ADD] authentification dans le routage

--- Router/Route.js
+++ Router/Route.js
@@ -1,8 +1,18 @@
 export default class Route {
-    constructor(url, title, pathHtml, pathJS = "") {
+    constructor(url, title, pathHtml, authorize, pathJS = "") {
       this.url = url;
       this.title = title;
       this.pathHtml = pathHtml;
       this.pathJS = pathJS;
+      this.authorize = authorize;
     }
 }
+
+/**
+ [] -> Tout le monde peutt y accéder
+  ["disconnected"] --> page réservée aux utilisateur déconnectés
+  ["employé"] --> page réservée aux utilisateur employés
+  ["vétérinaire"] --> page réservée aux utilisateur vétérinaire
+  ["admin"] --> page réservée aux utilisateur admin
+  ["admin", "vétérinaire", "employé"] --> page réservée aux utilisateur admin ou vétérinaire ou employé 
+ */
--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,21 +2,21 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/habitat", "Habitat", "/pages/habitat.html"),
-    new Route("/services", "Services", "/pages/services.html"),
-    new Route("/detailTigre", "La maison du tigre", "/pages/animaux/detailTigre.html"),
-    new Route("/detailZebre", "La maison du Zebre", "/pages/animaux/detailZebre.html"),
-    new Route("/detailGirafe", "La maison de la Girafe", "/pages/animaux/detailGirafe.html"),
-    new Route("/detailKoala", "La maison du Koala ", "/pages/animaux/detailKoala.html"),
-    new Route("/reservationRestauration", "détails de la Restauration", "/pages/services/restauration.html"),
-    new Route("/reservationBalade", "détails des balades", "/pages/services/balade.html"),
-    new Route("/reserverBalade", "reservation d'une Balade", "/pages/reservations/reserverBalade.html"),
-    new Route ("/ficheReservation", "fiche de reservation", "/pages/reservations/ficheReservation.html"),
-    new Route ("/signin", "Connexion", "/pages/auth/signin.html", "/js/auth/signin.js"),,
-    new Route ("/signup", "Inscription", "/pages/auth/signup.html", "/js/auth/signup.js"), 
-    new Route ("/gestionService", "gestion des services", "/pages/admin/gestionService.html"),
-    new Route("/reserver", "réserver une table", "/pages/reservations/reserver.html"),
+    new Route("/", "Accueil", "/pages/home.html", []),
+    new Route("/habitat", "Habitat", "/pages/habitat.html", []),
+    new Route("/services", "Services", "/pages/services.html", []),
+    new Route("/detailTigre", "La maison du tigre", "/pages/animaux/detailTigre.html", []),
+    new Route("/detailZebre", "La maison du Zebre", "/pages/animaux/detailZebre.html", []),
+    new Route("/detailGirafe", "La maison de la Girafe", "/pages/animaux/detailGirafe.html", []),
+    new Route("/detailKoala", "La maison du Koala ", "/pages/animaux/detailKoala.html", []),
+    new Route("/reservationRestauration", "détails de la Restauration", "/pages/services/restauration.html", []),
+    new Route("/reservationBalade", "détails des balades", "/pages/services/balade.html", []),
+    new Route("/reserverBalade", "reservation d'une Balade", "/pages/reservations/reserverBalade.html", []),
+    new Route ("/ficheReservation", "fiche de reservation", "/pages/reservations/ficheReservation.html", ["client"]),
+    new Route ("/signin", "Connexion", "/pages/auth/signin.html", ["disconnected"], "/js/auth/signin.js"),,
+    new Route ("/signup", "Inscription", "/pages/auth/signup.html", ["disconnected"], "/js/auth/signup.js"), 
+    new Route ("/gestionService", "gestion des services", ["admin"], "/pages/admin/gestionService.html"),
+    new Route("/reserver", "réserver une table", "/pages/reservations/reserver.html", []),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
